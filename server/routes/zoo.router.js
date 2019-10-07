6d1b4ffd353ff4534060f39f3f13fc86b8c69cdc
Edith Emmings <emmings.edith@gmail.com>
2019-10-07 13:14:46 -0500

    query written in server route

--- server/routes/zoo.router.js
+++ server/routes/zoo.router.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 router.get('/', (req, res) => {
-    // YOUR CODE HERE
+    let queryText = `SELECT "species".species_name, "class".class_name
+    FROM "species"
+    JOIN "class" on "class".id = "species".class_id;`
+    pool.query(queryText)
+        .then((result) => { res.send(result.rows); })
+        .catch((err) => {
+            console.log('Error completing SELECT movie query', err);
+            res.sendStatus(500);
+        });
     
 });
 
